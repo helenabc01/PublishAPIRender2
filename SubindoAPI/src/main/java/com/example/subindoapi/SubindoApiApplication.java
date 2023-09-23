3acb689ab13b15217ca1e8f557fa69a62ef882a6
Helena <helena.costa@germinare.org.br>
2023-09-23 18:25:32 -0300

    first commit

---
+++ SubindoAPI/src/main/java/com/example/subindoapi/SubindoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.subindoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubindoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubindoApiApplication.class, args);
+	}
+
+}
---
+++ SubindoAPI/src/test/java/com/example/subindoapi/SubindoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subindoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubindoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
